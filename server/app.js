e4896b64b980ccea6675dcaa3fcefeb330ef9e44
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-07 23:32:23 -0700

    updated routes

--- server/app.js
+++ server/app.js
@@ -6,10 +6,9 @@ const apiProxy = httpProxy.createProxyServer();
 const app = express();
 app.use(express.static(path.join(__dirname, '/../public/')));
 app.use('/:restaurant_id', express.static(path.join(__dirname, '/../public/')));
-var photos = 'http://13.57.252.210',
-    reservations = '18.217.25.48',
-    menus = 'http://18.219.221.244',
-    reviews = '54.153.40.76';
+// var photos = 'http://13.57.252.210',
+var reservations = 'http://18.217.25.48:3006',
+    reviews = 'http://54.153.40.76';
 
 // app.all("/:restaurantID/images", function(req, res) {
 //     console.log('redirecting to photo-gallery api');
