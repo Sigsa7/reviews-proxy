071772edae0160ba9e4a156eb87772f94a6170b9
Jennie Zeng <jenniezeng1@gmail.com>
2019-08-08 11:40:42 -0700

    redis cache for proxy on reviews routing

--- server/app.js
+++ server/app.js
@@ -1,14 +1,62 @@
 const express = require('express');
 const path = require('path');
-const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
+const redis = require('./redisConnection');
+const axios = require('axios');
+// const httpProxy = require('http-proxy');
+// const apiProxy = httpProxy.createProxyServer();
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/../public/')));
 app.use('/:restaurant_id', express.static(path.join(__dirname, '/../public/')));
-// var photos = 'http://13.57.252.210',
-var reservations = 'http://18.217.25.48',
-    reviews = 'http://54.153.40.76';
+
+const reservations = 'http://18.217.25.48';
+const reviews = 'http://54.153.40.76';
+
+app.get('/:restaurantId/reviews/reviewListing', (req, res) => {
+  const { restaurantId } = req.params;
+  const { sort, keywords, star } = req.query;
+
+  const redisQueryStr = `${reviews}/${restaurantId}/reviews/reviewListing/${sort}/${keywords}/${star}`;
+  
+  redis.get(redisQueryStr, async (err, result) => {
+    if (result) {
+      const jsonResult = JSON.parse(result);
+      return res.status(200).json(jsonResult);
+    } else {
+      axios.get(
+        `${reviews}/${restaurantId}/reviews/reviewListing`, 
+        { params: { sort, keywords, star } },
+      )
+        .then(({ data }) => {
+          redis.setex(redisQueryStr, 1800, JSON.stringify(data));
+          return data;
+        })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(500).json(err));
+    }
+  });
+});
+
+app.get('/:restaurantId/reviews/restaurantInfo', (req, res) => {
+  const { restaurantId } = req.params;
+
+  const redisQueryStr = `${reviews}/${restaurantId}/reviews/restaurantInfo`;
+  
+  redis.get(redisQueryStr, async (err, result) => {
+    if (result) {
+      const jsonResult = JSON.parse(result);
+      return res.status(200).json(jsonResult);
+    } else {
+      axios.get(`${reviews}/${restaurantId}/reviews/restaurantInfo`)
+        .then(({ data }) => {
+          redis.setex(redisQueryStr, 1800, JSON.stringify(data));
+          return data;
+        })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(500).json(err));
+    }
+  });
+});
 
 // app.all("/:restaurantID/images", function(req, res) {
 //     console.log('redirecting to photo-gallery api');
@@ -22,21 +70,6 @@ app.all('/booking/reserved/:restaurantID', function(req, res) {
 app.all('/booking/create/:restaurantID', function(req, res) {
   console.log('redirecting to reservation api');
 apiProxy.web(req, res, {target: reservations});
-});
-
-// app.all("/:restaurant_id/menus", function(req, res) {
-//     console.log('redirecting to menus api');
-//     apiProxy.web(req, res, {target: menus});
-// });
-
-app.all("/:restaurantID/reviews/reviewListing", function(req, res) {
-  console.log('redirecting to reviews api');
-  apiProxy.web(req, res, {target: reviews});
-});
-
-app.all("/:restaurantID/reviews/restaurantInfo", function(req, res) {
-  console.log('redirecting to reviews api');
-  apiProxy.web(req, res, {target: reviews});
 });
 
 module.exports = app;
